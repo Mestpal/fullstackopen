9817946185b174246ebc5a5bbbd8cd0ba295b670
Manuel Estevez Palencia <manuestpal@gmail.com>
2025-07-04 17:37:08 +0200

    Exercise 2.2 courseinfo 7

--- part2/courseinfo/src/components/course.jsx
+++ part2/courseinfo/src/components/course.jsx
@@ -4,18 +4,26 @@ const Content = (props) => {
   return props.parts.map((part) => <Part key={part.id} part={part} />)
 }
 
-
 const Part = (props) => (
   <p>
     {props.part.name} {props.part.exercises}
   </p>
 )
 
+const Total = (props) => <p><b>Total of {props.total} exercises</b></p>
+
 const Course = ({course}) => {  
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total
+        total={
+          course.parts[0].exercises +
+          course.parts[1].exercises +
+          course.parts[2].exercises
+        }
+      />
     </div>
   )
 }
