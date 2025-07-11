772287eb628e4752457163a7afe8219f46d0302c
Manuel Estevez Palencia <manuestpal@gmail.com>
2025-07-11 17:29:30 +0200

    Exercise 2.9 Agenda 4

--- part2/agenda/src/App.jsx
+++ part2/agenda/src/App.jsx
@@ -1,13 +1,26 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([]) 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
+
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
+  const [filter, setFilter] = useState('')
 
   const onChangeName = (event) => setNewName(event.target.value);
   const onChangeNumber = (event) => setNewNumber(event.target.value);
+  const onChangeFilter = (event) => setFilter(event.target.value)
+
+  const filteredResults = () => persons.filter((person) => 
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
+  const getContacts = () => filter ? filteredResults(): persons
 
   const onSubmitForm = (event) => {
     event.preventDefault()
@@ -16,7 +29,7 @@ const App = () => {
     if (!names.includes(newName)) {      
       const newPerson = {
         name: newName,
-        phone: newNumber
+        number: newNumber
       }
 
       setPersons(persons.concat(newPerson))
@@ -27,9 +40,17 @@ const App = () => {
     }
   }
 
+  const Agenda = ({contacts}) => {    
+    return contacts.map((person) => <div key={person.name}><b>{person.name}</b> {person.number}<br/></div>)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filter} onChange={onChangeFilter}/>
+      </div>
+      <h2>Add a new</h2>
       <form onSubmit={onSubmitForm}>
         <div>
           name: <input value={newName} onChange={onChangeName}/>
@@ -42,7 +63,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <div key={person.name}><b>{person.name}</b> {person.phone}<br/></div> )}
+      <Agenda contacts={getContacts()}/>
     </div>
   )
 }
